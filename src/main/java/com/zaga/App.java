6a4d7fd3d77c8bf347e6ac02a0e207db96787c93
pratheebapc <pratheebaravindran@gmail.com>
2022-12-28 03:23:37 -0800

    new commit

---
+++ src/main/java/com/zaga/App.java
@@ -0,0 +1,20 @@
+package com.zaga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String args[])
+    {
+        int i = 20;
+ 
+        if (i < 15)
+            System.out.println("i is smaller than 15");
+        else
+            System.out.println("i is greater than 15");
+ 
+        System.out.println("Outside if-else block");
+    }
+}
